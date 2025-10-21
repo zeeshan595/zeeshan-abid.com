1dda4bf5cc01321c8164b53dd05e7f4e91b0ccfd
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2025-10-21 23:32:54 +0500

    better background colors

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/home';
 import NotFound from './pages/not-found';
+import { useEffect } from 'react';
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,22 @@ export const router = createBrowserRouter([
 ]);
 
 export default function () {
+  function getRandomColors() {
+    const randomSum = 610 + Math.round(Math.random() * 10);
+    const r = 200 + Math.round(Math.random() * 10);
+    const g = 200 + Math.round(Math.random() * 10);
+    const b = randomSum - (r + g);
+    return `rgba(${r}, ${g}, ${b}, 1)`;
+  }
+
+  // handle background color change
+  useEffect(() => {
+    const color1 = getRandomColors();
+    const color2 = getRandomColors();
+    document.body.style.backgroundColor = `${color2};`;
+    document.body.style.background = `linear-gradient(90deg, ${color1} 0%, ${color2} 100%)`;
+  }, []);
+
   return (
     <div
       className="mx-4 flex-col gap-4 my-18 items-center"
